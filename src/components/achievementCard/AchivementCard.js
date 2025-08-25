048dfad523458abb7a29b24e88b2a9da048ec339
Sourabh-Lowanshi <slowanshi21@gmail.com>
2025-08-25 13:57:48 +0530

    Seo

--- src/components/achievementCard/AchivementCard.js
+++ src/components/achievementCard/AchivementCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 export default function AchivementCard({ cardInfo }) {
   function openUrlInNewTab(url) {
@@ -7,19 +8,25 @@ export default function AchivementCard({ cardInfo }) {
   }
 
   return (
-    <div className="certificate-card">
-      <div className="certificate-image-div">
-        <img src={cardInfo.image} alt="PWA" className="card-image"></img>
+    <>
+      <Helmet>
+        <title>{cardInfo.title} | Achievement</title>
+        <meta name="description" content={cardInfo.description} />
+      </Helmet>
+      <div className="certificate-card">
+        <div className="certificate-image-div">
+          <img src={cardInfo.image} alt="PWA" className="card-image"></img>
+        </div>
+        <div className="certificate-detail-div">
+          <h5 className="card-title">{cardInfo.title}</h5>
+          <p className="card-subtitle">{cardInfo.description}</p>
+        </div>
+        <div className="certificate-card-footer">
+          {cardInfo.footer.map((v, i) => {
+            return <p onClick={() => openUrlInNewTab(v.url)}>{v.name}</p>;
+          })}
+        </div>
       </div>
-      <div className="certificate-detail-div">
-        <h5 className="card-title">{cardInfo.title}</h5>
-        <p className="card-subtitle">{cardInfo.description}</p>
-      </div>
-      <div className="certificate-card-footer">
-        {cardInfo.footer.map((v, i) => {
-          return <p onClick={() => openUrlInNewTab(v.url)}>{v.name}</p>;
-        })}
-      </div>
-    </div>
+    </>
   );
 }
--- src/containers/education/Educations.js
+++ src/containers/education/Educations.js
@@ -3,25 +3,36 @@ import "./Educations.css";
 import DegreeCard from "../../components/degreeCard/DegreeCard.js";
 import { degrees } from "../../portfolio";
 import { Fade } from "react-reveal";
+import { Helmet } from "react-helmet";
 
 class Educations extends Component {
   render() {
     const theme = this.props.theme;
     return (
-      <div className="main" id="educations">
-        <div className="educations-header-div">
-          <Fade bottom duration={2000} distance="20px">
-            <h1 className="educations-header" style={{ color: theme.text }}>
-              Degrees Received
-            </h1>
-          </Fade>
+      <>
+        {" "}
+        <Helmet>
+          <title>Educations | Sourabh Lowanshi</title>
+          <meta
+            name="description"
+            content="View the degrees and educational qualifications of Sourabh Lowanshi, a Full Stack Developer. Learn about his academic background."
+          />
+        </Helmet>
+        <div className="main" id="educations">
+          <div className="educations-header-div">
+            <Fade bottom duration={2000} distance="20px">
+              <h1 className="educations-header" style={{ color: theme.text }}>
+                Degrees Received
+              </h1>
+            </Fade>
+          </div>
+          <div className="educations-body-div">
+            {degrees.degrees.map((degree) => {
+              return <DegreeCard degree={degree} theme={theme} />;
+            })}
+          </div>
         </div>
-        <div className="educations-body-div">
-          {degrees.degrees.map((degree) => {
-            return <DegreeCard degree={degree} theme={theme} />;
-          })}
-        </div>
-      </div>
+      </>
     );
   }
 }
